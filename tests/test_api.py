7fff7aaa3775b91db5b45d60f4e347b2778d5521
Julien Cartigny <julien.iguchi-cartigny@univ-lille1.fr>
2017-07-23 16:52:15 +0200

    Tests use extra ovh_client parameter

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,13 @@
+import ovh
 import pytest
 
 from ovhcloud.client import OVHClient
 
+test_data = ovh.client.ENDPOINTS.keys()
 
-def test_cache_command():
+
+@pytest.mark.parametrize("endpoint", test_data)
+def test_cache_command(endpoint):
+    ovh_client = ovh.Client(endpoint=endpoint)
     with pytest.raises(SystemExit):
-        OVHClient(['cache']).action()
+        OVHClient(['cache'], ovh_client).action()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,8 +1,10 @@
+import ovh
 import pytest
 
 from ovhcloud.client import OVHClient
 
 
 def test_version_command():
+    ovh_client = ovh.Client(endpoint='ovh-eu')
     with pytest.raises(SystemExit):
-        OVHClient(['version']).action()
+        OVHClient(['version'], ovh_client=ovh_client).action()
